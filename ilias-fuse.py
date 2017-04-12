967c631e9691672ee2f627af672529e4c4ca4927
Janis Streib <janis@dogcraft.de>
2017-04-13 00:45:05 +0200

    FIX: also fixed "heute" m)

--- ilias-fuse.py
+++ ilias-fuse.py
@@ -259,6 +259,7 @@ class File(IliasNode):
         self.size = self.human2bytes(properties[1].text)  # Approximate file size
         with setlocale('de_DE.UTF-8'):
             date_string = properties[2].text.strip().lower()
+            date_string = date_string.replace('heute', (datetime.datetime.now()).strftime('%d. %b %Y')) if 'heute' in date_string else date_string
             date_string = date_string.replace('gestern', (datetime.datetime.now() - datetime.timedelta(
                 days=1)).strftime('%d. %b %Y')) if 'gestern' in date_string else date_string
             dtime = datetime.datetime.strptime(date_string, "%d. %b %Y, %H:%M")  # TODO Timezone?
